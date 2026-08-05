804ee6fd9e84ea4f9f66f8ad16d748fe6ae9cb75
Thuany <thuany.g19@aluno.ifsc.edu.br>
2026-08-04 21:09:09 -0300

    Exercicio 5

---
+++ Atv00/src/ex5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ex5 {
+
+    static double tarifaBase = 2;
+
+    static void main(){
+
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite" + "quantas horas você ficou" + "no estacionamento:");
+        double horas = leia.nextDouble();
+
+        System.out.println(Math.ceil(horas));
+
+        double valorPagar = calcularEncargos(horas, tarifaBase);
+        System.out.println("Valor a pagar é R$ " + valorPagar);
+
+    }
+    private static double calcularEncargos(Double horas, double tarifaBase){
+        double valorAdicional = 0.5;
+
+        if (horas <= 3){
+            return tarifaBase;
+        } else {
+            return tarifaBase + (valorAdicional * (Math.ceil(horas)-3));
+        }
+    }
+}
