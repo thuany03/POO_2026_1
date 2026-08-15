c9059589f9c54b6d14838ebc16fa092ef0808c19
Thuany <thuany.g19@aluno.ifsc.edu.br>
2026-08-14 22:01:59 -0300

    Jogo da velha

--- Atv01/src/ListaJogos.java
+++ Atv01/src/ListaJogos.java
@@ -4,19 +4,16 @@ public class ListaJogos {
 
 
     static Scanner leia = new Scanner(System.in);
+    static int tamanho = 10;
+    static String[] jogos = new String[tamanho];
 
     static void main() {
-
-
-        int tamanho = 10;
-
-        String[] jogos = new String[tamanho];
 
         int opcao = 0;
 
         do {
-            System.out.println("Digite uma opção:");
-            System.out.println("1. Cadastrar Jogo");
+            System.out.println("Digite o número da sua opção:");
+            System.out.println("1. Cadastrar Jogo"); 
             System.out.println("2. Listar Jogos");
             System.out.println("3. Atualizar Jogo");
             System.out.println("4. Remover Jogo");
@@ -28,19 +25,19 @@ public class ListaJogos {
 
             switch (opcao) {
                 case 1:
-                    CadastrarJogo(jogos, tamanho);
+                    CadastrarJogo();
                     break;
 
                 case 2:
-                    ListarJogos(jogos);
+                    ListarJogos();
                     break;
 
                 case 3:
-                    AtualizarJogo(jogos);
+                    AtualizarJogo();
                     break;
 
                 case 4:
-                    RemoverJogo(jogos);
+                    RemoverJogo();
                     break;
 
                 case 5:
@@ -51,16 +48,16 @@ public class ListaJogos {
 
     }
 
-    private static void CadastrarJogo(String[] jogos, int tamanho) {
+    private static void CadastrarJogo() {
 
         for(int i = 0; i < tamanho; i++){
             if(jogos[i]==null){
                 System.out.println("Digite o nome do jogo:");
                 String nomeJogo = leia.nextLine();
 
-                if(JaEstaNaLista(jogos, i, nomeJogo)){
-                    System.out.println("Este jogo já está na lista");
-                    break;
+                if(JaEstaNaLista(nomeJogo) != -1){
+                    System.out.println("Este jogo já está na lista.");
+                    return;
                 } else {
                     jogos[i] = nomeJogo;
                     System.out.println("Jogo adicionado com sucesso!");
@@ -68,26 +65,40 @@ public class ListaJogos {
                 }
             }
         }
-        System.out.println("A lista de jogos está cheia");
+        System.out.println("A lista de jogos está cheia.");
     }
 
-    private static boolean JaEstaNaLista(String[] jogos, int tamanho, String nomeJogo){
+    private static int JaEstaNaLista(String nomeJogo2){
         for(int i = 0; i < tamanho; i++){
-        if(jogos[i].equals(nomeJogo))
-            return true;
-         }
-        return false;
+            if( (jogos[i] != null) && (jogos[i].equals(nomeJogo2))){
+                return i;
+            }
+        }
+        return -1;
     }
 
-    private static void ListarJogos(String[] jogos) {
+    private static void ListarJogos() {
+        for (int i = 0; i < jogos.length; i++){
+            if(jogos[i] != null){
+                System.out.println(jogos[i]);
+            }
+
+        }
+
 
     }
 
-    private static void AtualizarJogo(String[] jogos) {
+    private static void AtualizarJogo() {
+        System.out.println("Digite o nome do jogo que deseja atualizar:");
+        String nomeJogo = leia.nextLine();
+
+        if(JaEstaNaLista(nomeJogo) == -1){
+            System.out.println("Jogo inexistente na lista.");
+        }
 
     }
 
-    private static void RemoverJogo(String[] jogos) {
+    private static void RemoverJogo() {
 
     }
 }
